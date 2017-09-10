986c2e6c9b39a6a18c6f87e56e5aa4e3ef424808
Laksh <laksharora28@gmail.com>
2017-09-10 23:51:19 +0530

    flask site up, word cloud graph done

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import json
+
+from flask import Flask, render_template, jsonify
+
+
+app = Flask(__name__)
+app.secret_key = 's0mth1ng s3cr3t'
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/geoplot')
+def geoplot():
+    try:
+        return render_template('geoplot.html')
+    except:
+        return "Seems like developer has not updated the site yet! Contact => <a href='//github.com/techedlaksh'> Laksh"
+
+@app.route('/popcat')
+def popcat():
+    try:
+        return render_template('popcat.html')
+    except:
+        return "Seems like developer has not updated the site yet! Contact => <a href='//github.com/techedlaksh'> Laksh"
+
+
+@app.route('/wordcloud')
+def wordcloud():
+    try:
+        return render_template('wordcloud.html')
+    except:
+        return "Seems like developer has not updated the site yet! Contact => <a href='//github.com/techedlaksh'> Laksh"
+
+
+@app.route('/posneg')
+def posneg():
+    try:
+        return render_template('posneg.html')
+    except:
+        return "Seems like developer has not updated the site yet! Contact => <a href='//github.com/techedlaksh'> Laksh"
+
+
+@app.route('/get/<file>')
+def get(file):
+    if file in ['word_freq']:
+        with open('static/data/'+file+'.json') as f:
+            data = json.load(f)
+        return jsonify(data)
+    else:
+        return 'File not Found !'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
