82ca0469773c93000cfef656fdbef523ffdea725
Laksh <laksharora28@gmail.com>
2017-09-12 14:19:12 +0530

    heroku changes

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def get(file):
         return 'File not Found !'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
